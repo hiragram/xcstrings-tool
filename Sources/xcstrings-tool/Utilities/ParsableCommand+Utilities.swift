fc63d1ce9e3fa9b85e5da862a774f36dffd5b849
Yuya Hirayama <hiragram@gmail.com>
2024-01-04 06:48:52 +0900

    Add debug note

--- Sources/xcstrings-tool/Utilities/ParsableCommand+Utilities.swift
+++ Sources/xcstrings-tool/Utilities/ParsableCommand+Utilities.swift
@@ -12,7 +12,11 @@ extension ParsableCommand {
         let fileManager = FileManager.default
 
         if !fileManager.fileExists(atPath: directoryURL.path(percentEncoded: false)) {
+            note("Creating directory at \(directoryURL)")
             try fileManager.createDirectory(at: directoryURL, withIntermediateDirectories: true)
+            note("Created directory at \(directoryURL)")
+        } else {
+            note("No need to create directory.")
         }
     }
 }
