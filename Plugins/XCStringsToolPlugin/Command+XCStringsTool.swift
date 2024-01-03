b86b0af5183bb4dee6f13be1b857084b4434a6d2
Yuya Hirayama <hiragram@gmail.com>
2024-01-04 08:58:20 +0900

    wip

--- Plugins/XCStringsToolPlugin/Command+XCStringsTool.swift
+++ Plugins/XCStringsToolPlugin/Command+XCStringsTool.swift
@@ -15,29 +15,43 @@ extension XcodePluginContext: PluginContextProtocol {}
 
 extension Command {
     static func xcstringstool(for file: File, using context: PluginContextProtocol) throws -> Command {
-        let command = Command.buildCommand(
-            displayName: "XCStringsTool: Generate Swift code for ‘\(file.path.lastComponent)‘",
-            executable: try context.tool(named: "xcstrings-tool").path,
-            arguments: [
-                file.path,
-                context.outputPath(for: file)
-            ],
-            inputFiles: [
-                file.path
-            ],
-            outputFiles: [
-                context.outputPath(for: file)
-            ]
-        )
-        print("mowamowa: \(command)")
-        print("env: \(ProcessInfo.processInfo.environment["CI_XCODE_CLOUD"])")
-        return command
+        let isRunningOnXcodeCloud = ProcessInfo.processInfo.environment["CI_XCODE_CLOUD"] == "TRUE"
+
+        let displayName = "XCStringsTool: Generate Swift code for ‘\(file.path.lastComponent)‘"
+        let executablePath = try context.tool(named: "xcstrings-tool").path
+        let arguments: [CustomStringConvertible] = [
+            file.path,
+            context.outputPath(for: file)
+        ]
+
+        if isRunningOnXcodeCloud {
+            print("mowamowa: XcodeCloud!")
+            return .prebuildCommand(
+                displayName: displayName,
+                executable: executablePath,
+                arguments: arguments,
+                outputFilesDirectory: context.outputDirectory
+            )
+        } else {
+            print("mowamowa: Not XcodeCloud!")
+            return .buildCommand(
+                displayName: displayName,
+                executable: executablePath,
+                arguments: arguments,
+                inputFiles: [
+                    file.path
+                ],
+                outputFiles: [
+                    context.outputPath(for: file)
+                ]
+            )
+        }
     }
 }
 
 private extension PluginContextProtocol {
     var outputDirectory: Path {
-        pluginWorkDirectory
+        pluginWorkDirectory.appending(subpath: "XCStringsTool")
     }
 
     func outputPath(for file: File) -> Path {
