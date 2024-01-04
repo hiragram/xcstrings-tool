48de4d948352474ddcda4cf0f54cdb6502bda717
Yuya Hirayama <hiragram@gmail.com>
2024-01-04 17:52:15 +0900

    wip

--- Plugins/XCStringsToolPlugin/Command+XCStringsTool.swift
+++ Plugins/XCStringsToolPlugin/Command+XCStringsTool.swift
@@ -38,7 +38,7 @@ extension Command {
 
 private extension PluginContextProtocol {
     var outputDirectory: Path {
-        pluginWorkDirectory
+        pluginWorkDirectory.appending(subpath: "GeneratedFiles")
     }
 
     func outputPath(for file: File) -> Path {
