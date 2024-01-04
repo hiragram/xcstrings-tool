f0a69b3169303c9fd147a216f472604b7375b039
Yuya Hirayama <hiragram@gmail.com>
2024-01-04 16:41:36 +0900

    Revert

--- Plugins/XCStringsToolPlugin/Command+XCStringsTool.swift
+++ Plugins/XCStringsToolPlugin/Command+XCStringsTool.swift
@@ -15,37 +15,20 @@ extension XcodePluginContext: PluginContextProtocol {}
 
 extension Command {
     static func xcstringstool(for file: File, using context: PluginContextProtocol) throws -> Command {
-        let isRunningOnXcodeCloud = ProcessInfo.processInfo.environment["CI_XCODE_CLOUD"] == "TRUE"
-
-        let displayName = "XCStringsTool: Generate Swift code for ‘\(file.path.lastComponent)‘"
-        let executablePath = try context.tool(named: "xcstrings-tool").path
-        let arguments: [CustomStringConvertible] = [
-            file.path,
-            context.outputPath(for: file)
-        ]
-
-        if isRunningOnXcodeCloud {
-            print("mowamowa: XcodeCloud!")
-            return .prebuildCommand(
-                displayName: displayName,
-                executable: executablePath,
-                arguments: arguments,
-                outputFilesDirectory: context.outputDirectory
-            )
-        } else {
-            print("mowamowa: Not XcodeCloud!")
-            return .buildCommand(
-                displayName: displayName,
-                executable: executablePath,
-                arguments: arguments,
-                inputFiles: [
-                    file.path
-                ],
-                outputFiles: [
-                    context.outputPath(for: file)
-                ]
-            )
-        }
+        .buildCommand(
+            displayName: "XCStringsTool: Generate Swift code for ‘\(file.path.lastComponent)‘",
+            executable: try context.tool(named: "xcstrings-tool").path,
+            arguments: [
+                file.path,
+                context.outputPath(for: file)
+            ],
+            inputFiles: [
+                file.path
+            ],
+            outputFiles: [
+                context.outputPath(for: file)
+            ]
+        )
     }
 }
 
